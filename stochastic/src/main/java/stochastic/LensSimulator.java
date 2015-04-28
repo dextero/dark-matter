b57b722aeee82036b88718d3529ae4c1e4ca626a
dextero <hukutizuviki+git@gmail.com>
2015-04-28 23:32:06 +0200

    Add multiple test scenarios to main

--- stochastic/src/main/java/stochastic/LensSimulator.java
+++ stochastic/src/main/java/stochastic/LensSimulator.java
@@ -7,14 +7,16 @@ import java.util.List;
 
 public class LensSimulator {
 	public static void main(String[] args) {
-        List<Lens> lenses = new ArrayList<>();
-        lenses.add(new Lens(new Vector3D(1.0, 1.0, 1.0), 100.0, 10.0));
+        for (double x = -3.0; x <= 3.0; x += 1.5) {
+            List<Lens> lenses = new ArrayList<>();
+            lenses.add(new Lens(new Vector3D(x, x, 1.0), 100.0, 10.0));
 
-        LensSimulation sim = new LensSimulation(lenses);
-        Ray input = new Ray(new Vector3D(0.0, 0.0, 0.0),
-                            new Vector3D(0.0, 0.0, 1.0));
-        Ray output = sim.simulate(input);
-        System.out.println("input = " + input);
-        System.out.println("output = " + output);
+            LensSimulation sim = new LensSimulation(lenses);
+            Ray input = new Ray(new Vector3D(0.0, 0.0, 0.0), new Vector3D(0.0, 0.0, 1.0));
+            Ray output = sim.simulate(input);
+            System.out.println("lens at: " + lenses.get(0));
+            System.out.println("input = " + input);
+            System.out.println("output = " + output);
+        }
     }
 }
